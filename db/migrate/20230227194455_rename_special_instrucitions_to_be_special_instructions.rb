4c159edfc0220d08e1c66bf697e1884c648ad016
Jerome Million Rousseau <jerome@nebula-tales.com>
2023-02-28 11:52:43 +0000

    Cleaned the migration folder.

--- db/migrate/20230227194455_rename_special_instrucitions_to_be_special_instructions.rb
+++
@@ -1,5 +0,0 @@
-class RenameSpecialInstrucitionsToBeSpecialInstructions < ActiveRecord::Migration[7.0]
-  def change
-    rename_column :pets, :special_instrucitions, :special_instructions
-  end
-end
