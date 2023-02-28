f05d1b67a47755d99e822d06225f47830c47fa54
cristian fuhrich <fuehrich@gmail.com>
2023-02-28 18:01:21 +0000

    booking

--- app/controllers/bookings_controller.rb
+++ app/controllers/bookings_controller.rb
@@ -10,6 +10,7 @@ class BookingsController < ApplicationController
   def new
     @booking = Bookng.new
   end
+  
   def create
     @booking = Booking.new(pet_params)
     @booking.user = current_user
@@ -45,6 +46,4 @@ class BookingsController < ApplicationController
   def booking_params
     params.require(:pet).permit(:name, :species, :sex, :neutered, :age, :special_instructions)
   end
-end
-
 end
