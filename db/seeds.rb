170594f485139589df4a6ff0ad4cf4837b28ad70
cristian fuhrich <fuehrich@gmail.com>
2023-03-02 12:20:55 +0000

    search bar

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,6 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+
+# install require yaml and open-uri
+require "open-uri"
+require "yaml"
 require 'faker'
 
 puts 'Cleaning the databases...'
